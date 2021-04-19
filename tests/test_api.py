31de6c6274041028f7aeeadc761673c9aac98073
Mar Jene <mar.jene@somenergia.coop>
2021-04-19 12:55:42 +0200

    add test for check_erp_conn_dependency

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,3 +15,10 @@ class TestApi:
         assert response.status_code == 200
         assert response.json() == [{'model': "poweremail.templates", 'name': 'Template test', 'template_id': None, 'xml_id': 'template_module.template_01'},
                                    {'model': "poweremail.templates", 'name': 'Template2 test', 'template_id': None, 'xml_id': 'template_module.template_02'}]
+
+    async def test_check_erp_conn_dependency(self, test_app):
+        async with AsyncClient(app=test_app, base_url="http://test") as ac:
+            response = await ac.get("/templates/1")
+
+        assert response.status_code == 503
+        assert response.text == '{"detail":"Can\'t connect to ERP"}'
