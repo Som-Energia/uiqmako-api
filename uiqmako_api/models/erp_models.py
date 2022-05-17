8ab8ce04bb7234f0bacb7c70ad43606a52f2da93
Mar Jene <mar.jene@somenergia.coop>
2022-05-17 17:15:09 +0200

    upload same text to all languages

--- uiqmako_api/models/erp_models.py
+++ uiqmako_api/models/erp_models.py
@@ -10,6 +10,7 @@ class PoweremailTemplates:
     def __init__(self, ERP, xml_id, erp_id=None):
 
         self._PoweremailTemplates = ERP['poweremail.templates']
+        self._erp = ERP
         if xml_id:
             erp_id = ERP.get_erp_id(xml_id=xml_id, expected_model='poweremail.templates')
         elif not erp_id:
@@ -24,4 +25,20 @@ class PoweremailTemplates:
             key: template_fields[key]
             for key in self._editable_fields
         }
-        return self._PoweremailTemplates.write(self.id, fields_to_write)
+        self._PoweremailTemplates.write(self.id, fields_to_write)
+        return IrTranslation(self._erp).upload_translation_all(fieldname='def_body_text', model='poweremail.templates', res_id=self.id, value=body_text)
+
+
+
+class IrTranslation:
+    _IrTranslation = None
+    _fields = ['id', 'name', 'lang', 'value', 'res_id', 'src']
+    _erp = None
+
+    def __init__(self, ERP):
+        self._IrTranslation = ERP['ir.translation']
+        self._erp
+
+    def upload_translation_all(self, fieldname, model, res_id, value):
+        tr_ids = self._IrTranslation.search([('res_id', '=', res_id), ('name', '=', '{},{}'.format(model,fieldname))])
+        return self._IrTranslation.write(tr_ids, {'value': value})
