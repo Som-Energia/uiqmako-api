893c5b6eb0232000ab603d3f8fa68edc69dfe8c3
Xavier Terés <66311413+xavierteres@users.noreply.github.com>
2022-08-11 14:57:52 +0200

    add database config to .env file

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
 import dotenv
 
+class DatabaseConnectionData(BaseModel):
+    NAME: str
+    HOST: str
+    PORT: int
+    USER: str
+    PWD: str
+
 class APIBaseSettings(BaseSettings):
-    DB_NAME: str
+    DB_CONFIG: DatabaseConnectionData
     ACCESS_TOKEN_EXPIRE_HOURS: int
     ALGORITHM: str
     SECRET_KEY: str
