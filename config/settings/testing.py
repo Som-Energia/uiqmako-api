aa4d367a27c3f985a5ddb87fe9836c603f2bf9bd
David García Garzón <david.garcia@somenergia.coop>
2022-08-26 22:17:25 +0200

    now testing configuration must include erp config

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from .base import APIBaseSettings
+from .local import LocalSettings
 
 
-class TestingSettings(APIBaseSettings):
+class TestingSettings(LocalSettings):
+
     class Config:
         env_file = 'tests/.env.test'
         env_file_encoding = 'utf-8'
 
     def __init__(self):
         super(TestingSettings, self).__init__()
-
 
 settings = TestingSettings()
